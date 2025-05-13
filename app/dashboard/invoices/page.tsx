46cbd8559c1dcffcd8649141c7d2072598e74760
Aurelio Ndilou <marty.aurel@gmail.com>
2025-05-13 19:53:42 -0400

    chapter 16

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,5 @@
+import type { Metadata } from 'next';
+
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import { fetchInvoicesPages } from '@/app/lib/data';
@@ -6,6 +8,9 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+export const metadata: Metadata = {
+    title: 'Invoices',
+};
 
 export default async function Page(props: {
     searchParams?: Promise<{
